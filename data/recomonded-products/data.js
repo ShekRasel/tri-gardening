ec31f19eb3dd53c8951e4648d7423d5fd2f6bf88
Shek Rasel <swe.rasel@gmail.com>
2025-11-10 10:49:04 +0600

    new data

---
+++ data/recomonded-products/data.js
@@ -0,0 +1,38 @@
+// Mock "You may also like" products
+export const recommendedProducts = [
+  {
+    id: 2,
+    image: "/images/all-products/Snake Plant.avif",
+    name: "Snake Plant",
+    price: "1500 - 2800",
+    tag: "Indoor Plant",
+  },
+  {
+    id: 3,
+    image: "/images/all-products/Lavender.avif",
+    name: "Fiddle Leaf Fig",
+    price: "3200 - 4500",
+    tag: "Indoor Plant",
+  },
+  {
+    id: 4,
+    image: "/images/all-products/Bamboo plant.avif",
+    name: "Golden Pothos",
+    price: "800 - 1800",
+    tag: "Climbing Plant",
+  },
+  {
+    id: 5,
+    image: "/images/all-products/Snake Plant.avif",
+    name: "Snake Plant",
+    price: "1500 - 2800",
+    tag: "Indoor Plant",
+  },
+  {
+    id: 6,
+    image: "/images/all-products/Lavender.avif",
+    name: "Fiddle Leaf Fig",
+    price: "3200 - 4500",
+    tag: "Decorative Plant",
+  },
+];
---
+++ data/related-products/data.js
@@ -0,0 +1,34 @@
+export const relatedProducts = [
+  {
+    id: 41,
+    image: "/images/featured-product/Monstera Deliciosa.jpg",
+    name: "Monstera Deliciosa",
+    tag: "Indoor Plant",
+    price: "2000 - 3400",
+    rating: 24,
+  },
+  {
+    id: 42,
+    image: "/images/featured-product/Organic Compost Fertilizer.jpg",
+    name: "Organic Compost Fertilizer",
+    tag: "Fertilizer",
+    price: "60 - 440",
+    rating: 12,
+  },
+  {
+    id: 43,
+    image: "/images/featured-product/Pruning Shears.jpg",
+    name: "Pruning Shears",
+    tag: "Equipment",
+    price: "180 - 450",
+    rating: 34,
+  },
+  {
+    id: 44,
+    image: "/images/featured-product/Snake Plant.jpg",
+    name: "Snake Plant",
+    tag: "Indoor Plant",
+    price: "150 - 500",
+    rating: 29,
+  },
+];
